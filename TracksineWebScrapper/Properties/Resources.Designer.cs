0fd97eaf40f23a331ef0f50d4349fb03efe9445d
eymenefealtun <eymenefealtun@hotmail.com>
2023-09-06 12:43:01 +0300

    refactor: name refactored

--- TracksineWebScrapper/Properties/Resources.Designer.cs
+++ TracksineWebScrapper/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TracksineWebScrapper.Properties {
+namespace TracksinoWebScraper.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TracksineWebScrapper.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TracksineWebScrapper.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TracksinoWebScraper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
